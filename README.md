4f04aa12fa10528780f041efc7e4c86a4ad3d24e
therealblue24 <99767624+therealblue24@users.noreply.github.com>
2025-08-30 19:33:26 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# `zz`: C utilites
+
+Gives you stuff like dynamic list/arrays and hash maps.
+
+Named `zz` because implementing that stuff per project will make you go zzz.
+
+Inspired by stuff found [here](https://github.com/RealNeGate/Cuik/tree/master/common).
+
+## Stuff done
+
+<a name="libs"></a>
+header | use |
+-------|-----|
+**[base.h](base.h)**, **[base.c](base.c)** | base library
+**[list.h](list.h)** | dynamic lists
+
+
+More to come!
---
+++ list.h
@@ -0,0 +1,101 @@
+// Copyright (C) 2025 therealblue24 under the MIT license.
+#ifndef LIST_H_
+#define LIST_H_
+
+#include <stddef.h>
+#include <stdlib.h>
+
+#include "base.h"
+
+// function that given a list size returns a new size to fit more elements
+// used for resizing list
+#ifndef LIST_NEW_SIZE
+#define LIST_NEW_SIZE(size) ((size) + ((size) / 2))
+#endif
+
+#ifndef LIST_DEFAULT_SIZE
+#define LIST_DEFAULT_SIZE (64)
+#endif
+
+// list header
+typedef struct list_hdr {
+	// how many elements in the list
+	size_t size;
+	// how much allocated memory for the list (capacity)
+	size_t cap;
+	// data (can be really any type, we are using VLAs anyway)
+	uint8_t data[];
+} list_hdr_t;
+
+// syntax sugar
+// clang-format off
+#define LIST(type) type*
+// clang-format on
+// the irony
+
+#endif /* LIST_H_ */
+
+static list_hdr_t *list_hdr(void *list)
+{
+	return ((list_hdr_t *)list) - 1;
+}
+
+static void *list_data(list_hdr_t *hdr)
+{
+	return &hdr->data[0];
+}
+
+// Makes a list. `count` is initial capacity, `size` is the size
+// of the type
+static void *list_donotuse_make(size_t count, size_t size)
+{
+	// LSP thinks this is C++.
+	list_hdr_t *hdr = (list_hdr_t *)zalloc((count * size) + sizeof(list_hdr_t));
+
+	hdr->cap = count;
+	hdr->size = 0;
+
+	return list_data(hdr);
+}
+
+// Deletes a list.
+static void list_donotuse_delete(void *list)
+{
+	free(list_hdr(list));
+	return;
+}
+
+// Resizes/reserves space of list, using `count` as cap */
+static void *list_donotuse_reserve(void *list, size_t count, size_t size)
+{
+	list_hdr_t *newhdr = (list_hdr_t *)zrealloc(
+		list_hdr(list), (count * size) + sizeof(list_hdr_t));
+
+	newhdr->cap = count;
+
+	return list_data(newhdr);
+}
+
+// reserve but only if hdr->size + count > hdr->cap
+static void *list_donotuse_fit(void *list, size_t count, size_t size)
+{
+	list_hdr_t *hdr = list_hdr(list);
+	if(hdr->size + count > hdr->cap) {
+		return list_donotuse_reserve(list, count, size);
+	}
+	return list;
+}
+
+// Makes a list with type `type`
+#define list_make(type) list_donotuse_make(LIST_DEFAULT_SIZE, sizeof(type))
+
+// Deletes a list
+#define list_delete(list) list_donotuse_delete((list))
+
+// Appends an element `elem` to list `list`
+#define list_append(list, elem)                               \
+	do {                                                      \
+		(list) = list_donotuse_fit((list), 1, sizeof(*list)); \
+		list_hdr_t *hdr = list_hdr((list));                   \
+		(list)[hdr->size++] = elem;                           \
+	} while(0);
