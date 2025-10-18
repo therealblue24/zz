233332256027a2796742c82f3a38cabcfd36d5b2
therealblue24 <99767624+therealblue24@users.noreply.github.com>
2025-10-17 20:23:32 -0400

    make based

--- base.c
+++ base.c
@@ -9,32 +9,32 @@
 /* calloc(1, size) with error checking */
 void *zalloc(size_t size)
 {
-	void *mem = malloc(size);
-	ASSERT(mem, "failed to allocate %zu bytes of memory", size);
-	memset(mem, 0, size);
-	return mem;
+    void *mem = malloc(size);
+    ASSERT(mem, "failed to allocate %zu bytes of memory", size);
+    memset(mem, 0, size);
+    return mem;
 }
 
 /* calloc(count, size) with error checking */
 void *zcalloc(size_t count, size_t size)
 {
-	void *mem = calloc(count, size);
-	ASSERT(mem, "failed to allocate %zux%zu bytes of memory", size, count);
-	return mem;
+    void *mem = calloc(count, size);
+    ASSERT(mem, "failed to allocate %zux%zu bytes of memory", size, count);
+    return mem;
 }
 
 /* realloc(ptr, newsize) with error checking */
 void *zrealloc(void *ptr, size_t newsize)
 {
-	void *newptr = realloc(ptr, newsize);
-	ASSERT(newptr, "failed to reallocate %zu bytes of memory", newsize);
-	return newptr;
+    void *newptr = realloc(ptr, newsize);
+    ASSERT(newptr, "failed to reallocate %zu bytes of memory", newsize);
+    return newptr;
 }
 
 /* duplicates memory `mem` with size `size` */
 void *memdup(void *mem, size_t size)
 {
-	void *newmem = zalloc(size);
-	memcpy(newmem, mem, size);
-	return newmem;
+    void *newmem = zalloc(size);
+    memcpy(newmem, mem, size);
+    return newmem;
 }
--- list.h
+++ list.h
@@ -1,4 +1,4 @@
-// Copyright (C) 2025 therealblue24 under the MIT license.
+/* Copyright (C) 2025 therealblue24 under the MIT license. */
 #ifndef LIST_H_
 #define LIST_H_
 
@@ -7,8 +7,8 @@
 
 #include "base.h"
 
-// function that given a list size returns a new size to fit more elements
-// used for resizing list
+/* function that given a list size returns a new size to fit more elements */
+/* used for resizing list */
 #ifndef LIST_NEW_SIZE
 #define LIST_NEW_SIZE(size) (2 * (size))
 #endif
@@ -17,87 +17,92 @@
 #define LIST_DEFAULT_SIZE (64)
 #endif
 
-// list header
+/* list header */
 typedef struct list_hdr {
-	// how many elements in the list
-	size_t size;
-	// how much allocated memory for the list (capacity)
-	size_t cap;
-	// data (can be really any type, we are using VLAs anyway)
-	uint8_t data[];
+    /* how many elements in the list */
+    size_t size;
+    /* how much allocated memory for the list (capacity) */
+    size_t cap;
+    /* data (can be really any type, we are using VLAs anyway) */
+    uint8_t data[];
 } list_hdr_t;
 
-// syntax sugar
+/* syntax sugar */
 // clang-format off
 #define LIST(type) type*
 // clang-format on
-// the irony
+/* the irony */
 
 #endif /* LIST_H_ */
 
 static list_hdr_t *list_hdr(void *list)
 {
-	return ((list_hdr_t *)list) - 1;
+    return ((list_hdr_t *)list) - 1;
 }
 
 static void *list_data(list_hdr_t *hdr)
 {
-	return &hdr->data[0];
+    return &hdr->data[0];
 }
 
-// Makes a list. `count` is initial capacity, `size` is the size
-// of the type
+/* Makes a list. `count` is initial capacity, `size` is the size
+ * of the type. */
 static void *list_donotuse_make(size_t count, size_t size)
 {
-	// LSP thinks this is C++.
-	list_hdr_t *hdr = (list_hdr_t *)zalloc((count * size) + sizeof(list_hdr_t));
+    list_hdr_t *hdr = zalloc((count * size) + sizeof(list_hdr_t));
 
-	hdr->cap = count;
-	hdr->size = 0;
+    hdr->cap = count;
+    hdr->size = 0;
 
-	return list_data(hdr);
+    return list_data(hdr);
 }
 
-// Deletes a list.
+/* Deletes a list. */
 static void list_donotuse_delete(void *list)
 {
-	free(list_hdr(list));
-	return;
+    free(list_hdr(list));
+    return;
 }
 
-// Resizes/reserves space of list, using `count` as cap */
+/* Resizes/reserves space of list, using `count` as cap */
 static void *list_donotuse_reserve(void *list, size_t count, size_t size)
 {
-	list_hdr_t *newhdr = (list_hdr_t *)zrealloc(
-		list_hdr(list), (count * size) + sizeof(list_hdr_t));
+    list_hdr_t *newhdr = (list_hdr_t *)zrealloc(
+        list_hdr(list), (count * size) + sizeof(list_hdr_t));
 
-	newhdr->cap = count;
+    newhdr->cap = count;
 
-	return list_data(newhdr);
+    return list_data(newhdr);
 }
 
-// reserve but only if hdr->size + count > hdr->cap
-// and reserves with LIST_NEW_SIZE(hdr->size + count)
+/* reserve but only if hdr->size + count > hdr->cap */
+/* and reserves with LIST_NEW_SIZE(hdr->size + count) */
 static void *list_donotuse_fit(void *list, size_t count, size_t size)
 {
-	list_hdr_t *hdr = list_hdr(list);
-	if(hdr->size + count > hdr->cap) {
-		return list_donotuse_reserve(list, LIST_NEW_SIZE(hdr->size + count),
-									 size);
-	}
-	return list;
+    list_hdr_t *hdr = list_hdr(list);
+    if(hdr->size + count > hdr->cap) {
+        return list_donotuse_reserve(list, LIST_NEW_SIZE(hdr->size + count),
+                                     size);
+    }
+    return list;
 }
 
-// Makes a list with type `type`
+/* Makes a list with type `type` */
 #define list_make(type) list_donotuse_make(LIST_DEFAULT_SIZE, sizeof(type))
 
-// Deletes a list
+/* Deletes a list */
 #define list_delete(list) list_donotuse_delete((list))
 
-// Appends an element `elem` to list `list`
+/* Appends an element `elem` to list `list` */
 #define list_append(list, elem)                               \
-	do {                                                      \
-		(list) = list_donotuse_fit((list), 1, sizeof(*list)); \
-		list_hdr_t *hdr = list_hdr((list));                   \
-		(list)[hdr->size++] = elem;                           \
-	} while(0);
+    do {                                                      \
+        (list) = list_donotuse_fit((list), 1, sizeof(*list)); \
+        list_hdr_t *hdr = list_hdr((list));                   \
+        (list)[hdr->size++] = elem;                           \
+    } while(0);
+
+/* list_size and list_len are the same. gets size/length of list */
+#define list_size(list) (list_hdr((list))->size)
+#define list_len(list) (list_hdr((list))->size)
+/* gets capacity of list */
+#define list_cap(list) (list_hdr((list))->cap)
